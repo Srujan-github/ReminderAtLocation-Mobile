95079419839fdd6aeb45e02daece0e4fc6a07d0b
thammishettisaisrujan123@gmail.com <thukuntlasaikiran117@gmail.com>
2024-09-15 13:01:18 +0530

    fun screen

--- app/src/main/java/com/example/reminderatlocation/MainActivity.kt
+++ app/src/main/java/com/example/reminderatlocation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.reminderatlocation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reminderatlocation.ui.theme.ReminderAtLocationTheme
@@ -17,12 +20,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReminderAtLocationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold { innerPadding ->
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(text = "Hello Location")
+                    }
                 }
             }
         }
